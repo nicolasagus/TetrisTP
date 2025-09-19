12aa588eef4a5f8b631b1564ddc1e4a1b0c94a34
nicolasagus <nicolasagustinalcaraz@gmail.com>
2025-09-19 00:11:02 +0000

    BoardTest

--- src/test/java/com/tetris/juego/BoardTest.java
+++ src/test/java/com/tetris/juego/BoardTest.java
@@ -22,4 +22,32 @@ public class BoardTest {
         }
         assertEquals(true, colision);
     }
+    @Test
+    public void test_colision(){
+        Board board = new Board ();
+        BasePiece PieceDog= new PieceDog(0,0);
+        board.addPiece(PieceDog);
+        assertEquals (board.getPieces(0),PieceDog);
+        boolean colision= false;
+        for (int i = 0; i < 19; i++) {
+            if(board.moveDownActivePiece()){
+                colision= true;
+                break;
+            }
+
+    }
+    }
+      @Test
+    public void noDebeIngresarMasPiezasQueElMaximo() {
+        Board board = new Board();
+        BasePiece pieceL = new PieceL(0,0);
+        board.addPiece(pieceL);
+        // Intentar agregar otra pieza mientras hay una activa
+        BasePiece otraPieza = new PieceL(0,0);
+        board.addPiece(otraPieza);
+        // Solo debe haber una pieza en el tablero
+        assertEquals(1, board.getPieces().size());
+    }
+
+
 }
